ade895da4195bb6a84be12d6ee761445a2ace4cc
Glitch (project-gitub) <none>
2021-12-08 23:21:11 +0000

    🍯📈 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+let randomNumer = 0;
 
+$(".play").click(function() {
+  let user = $("input").val();
+  $(".userChoice").text(user);
+  let randomNumber = Math.ceil(Math.random() * 3 )-1;
+  $(".computerChoice").text(choices[randomNumber]);
+});
+
+let choices=["rock","paper","scissors"];
+console.log(choices[20]);
